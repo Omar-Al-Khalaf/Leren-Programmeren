728ee91feb437e75f8026061b9d470b9c443cce5
OmarAlKhalaf <99047548@mydavinci.nl>
2023-01-24 13:20:36 +0100

    (Update on Part-1 foldre)

---
+++ Module-01/Part-1/Feestlunch.py
@@ -0,0 +1,10 @@
+# ==== Start ==== #
+
+croissants = 0.39
+stokbroden = 2.78
+kortingsbonnen = 1.50
+Total = 17 * croissants + 2 * stokbroden + - kortingsbonnen
+
+# ==== Coding ==== #
+
+print('You buy 17 croissants and 2 stokbroden and you have 1.50 worth of kortingsbonnen you Total is :', Total)
