ce971889511b8a6b4cb156ba8373cfb8c7363148
OmarAlKhalaf <99047548@mydavinci.nl>
2023-01-24 13:48:22 +0100

    (Update to Part-1 folder)

---
+++ Module-01/Part-1/Speelhal.py
@@ -0,0 +1,9 @@
+# ==== Start ==== #
+
+person = 7.45
+VipVR = 0.37
+TotalVip = 0.37 * 9
+Totalperson = 7.45 * 4
+Total = Totalperson + TotalVip
+# ==== Coding ==== #
+print('Welcome to Speelhal : you and 3 people + 45 minuten in de VIP-VR-GameSeat : you total is : ', Total)
